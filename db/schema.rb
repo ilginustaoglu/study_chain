55bb3fd3c7b820c9e338b7fec1bff05ca6df3cb6
ilginustaoglu <filginustaoglu@gmail.com>
2025-10-14 15:56:03 +0300

    we have only one button for pause and start in timers right now

--- db/schema.rb
+++ db/schema.rb
@@ -105,10 +105,10 @@ ActiveRecord::Schema.define(version: 2025_10_08_194544) do
     t.boolean "is_running", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "timer_type", default: "work", null: false
-    t.integer "target_duration", default: 1500
-    t.integer "pomodoro_count", default: 0, null: false
-    t.string "session_type", default: "work", null: false
+    t.string "timer_type"
+    t.integer "target_duration"
+    t.integer "pomodoro_count"
+    t.string "session_type"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
